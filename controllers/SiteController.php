8935ec015442db16cf3eb87d38619aa2bad79344
Dmitrii Vlasov <ring.72.dmitry@yandex.ru>
2023-07-13 13:28:23 +0200

    first commit

---
+++ controllers/SiteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+
+class SiteController
+{
+
+
+
+    public static function handleContact() {
+        return 'Handling submitted data';
+    }
+}
---
+++ views/contact.php
@@ -0,0 +1,20 @@
+<h1>Contact</h1>
+
+<form action="" method="post">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Subject</label>
+        <input type="text" name="subject" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email</label>
+        <input type="text" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Body</label>
+        <textarea name="body"></textarea>
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
