2216df3ad18020799f387a6eea0e71e8210e7868
Dmitrii Vlasov <ring.72.dmitry@yandex.ru>
2023-07-13 13:43:23 +0200

    Create Controller.php

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,11 +2,33 @@
 
 namespace app\controllers;
 
-class SiteController
+use app\core\Application;
+use app\core\Controller;
+
+class SiteController extends Controller
 {
+    /**
+     * @return void
+     */
+    public function home() {
 
+        $params = [
+          'name' => "Dear user"
+        ];
 
+        return $this->render('home', $params);
 
+    }
+
+    public function contact() {
+
+        return $this->render('contact');
+
+    }
+
+    /**
+     * @return string
+     */
     public static function handleContact() {
         return 'Handling submitted data';
     }
--- core/Router.php
+++ core/Router.php
@@ -55,13 +55,16 @@ class Router
 
         }
 
+        if(is_array($callback)) {
+            $instance = new $callback[0]();
+        }
         return call_user_func($callback);
     }
 
-    public function renderView($view) {
+    public function renderView($view, $params = []) {
 
         $layoutContent = $this->layoutContent();
-        $viewContent = $this->renderOnlyView($view);
+        $viewContent = $this->renderOnlyView($view, $params);
         return str_replace('{{content}}', $viewContent, $layoutContent);
         //include_once Application::$ROOT_DIR."/views/$view.php";
     }
@@ -78,8 +81,12 @@ class Router
         return ob_get_clean();
     }
 
-    protected function renderOnlyView($view)
+    protected function renderOnlyView($view, $params)
     {
+        foreach ($params as $key => $value) {
+            $$key = $value;
+        }
+
         ob_start();
         include_once Application::$ROOT_DIR."/views/$view.php";
         return ob_get_clean();
