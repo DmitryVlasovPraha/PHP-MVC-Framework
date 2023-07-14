5e76027aa6baa5bb958b36753b53167943a6f6eb
Dmitrii Vlasov <ring.72.dmitry@yandex.ru>
2023-07-14 10:04:52 +0200

    Create Form.php

--- models/RegisterModel.php
+++ models/RegisterModel.php
@@ -2,12 +2,30 @@
 
 namespace app\models;
 
-class RegisterModel
+use app\core\Model;
+
+class RegisterModel extends Model
 {
 
     public string $firstname;
     public string $lastname;
+    public string $email;
     public string $password;
-    public string $passwordConfirm;
+    public string $confirmPassword;
+
+    public function register() {
+        echo "New user";
+    }
+
+    public function rules(): array
+    {
+        return [
+            'firstname' => [self::RULE_REQUIRED],
+            'lastname' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 20]],
+            'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'math' => 'password']]
+        ];
+    }
 
 }
--- views/register.php
+++ views/register.php
@@ -5,14 +5,17 @@
         <div class="col">
             <div class="form-group">
                 <label for="exampleInputEmail1">First name</label>
-                <input type="text" name="firstname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="text" name="firstname" class="form-control  <?php echo $model->hasError('firstname') ? ' is-invalid' : '' ?>" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
         </div>
         <div class="col">
             <div class="form-group">
                 <label for="exampleInputEmail1">Last name</label>
-                <input type="text" name="lastname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <input type="text" name="lastname" class="form-control <?php echo $model->hasError('firstname') ? ' is-invalid' : '' ?>" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <div class="invalid-feedback">
+                    <?php echo $model->getFirstError('firstname') ?>
+                </div>
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
         </div>
